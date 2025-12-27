b6c101fe58481798ea68cfc794a83039f6ec4c0f
SanskarGandhewar <sanskargandhewar12345@gmail.com>
2025-12-27 21:42:12 +0530

    Initial commit - Student Management System

---
+++ Controllers/Employee/EmployeeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.Controllers.Employee
+{
+    public class EmployeeController : Controller
+    {
+        public ViewResult getListOfData()
+        {
+            var db = new EmpdbContext();
+            List<Emp> emp = db.Emps.ToList();
+            //Console.WriteLine(emp);
+            return View(emp);
+        }
+
+        [HttpGet]
+        public IActionResult AddEmp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmp(Emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new EmpdbContext();
+                db.Emps.Add(emp);
+                db.SaveChanges();
+                return RedirectToAction("getListOfData");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult EditEmp(int id) 
+        {
+            var db = new EmpdbContext();
+            Emp e = db.Emps.Find(id);
+            return View(e);
+        }
+
+        [HttpPost]
+        public IActionResult EditEmp(Emp emp)
+        {
+            var db = new EmpdbContext();
+            db.Emps.Update(emp);
+            db.SaveChanges();
+            return RedirectToAction("getListOfData");
+        }
+
+        public IActionResult DeleteEmp(int id) 
+        {
+            var db = new EmpdbContext();
+            Emp emp = db.Emps.Find(id);
+            db.Emps.Remove(emp);
+            db.SaveChanges();
+            return RedirectToAction("getListOfData");
+        }
+
+    }
+}
---
+++ Models/Emp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Models;
+
+public partial class Emp
+{
+    [Required(ErrorMessage ="Id is Required")]
+    public int Id { get; set; }
+    [StringLength(45,MinimumLength =3,ErrorMessage ="String should be 45 Character")]
+    public string? Ename { get; set; }
+    [Required (ErrorMessage ="Salary is required")]
+    [Range(500,100000,ErrorMessage = "Salary should between 500 to 100000")]
+    public float? Salary { get; set; }
+    [Required(ErrorMessage = "Address is required")]
+    public string? Address { get; set; }
+    [Required(ErrorMessage = "Gender is required")]
+    public string? Gender { get; set; }
+}
